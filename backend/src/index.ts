e6bf93c7854008a806fdc0ae6eaa789f5a3e8dcf
Nishant Raj <nishantanindian@gmail.com>
2025-02-16 16:47:39 +0530

    added api call from frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-
+import cors from 'cors';
 const app = express();
-
+app.use(cors());
+app.use(express.json());
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({ message: 'Hello World!' });
 });
 
 app.listen(3000, () => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
 
 
 function App() {
+   const [data, setData] = useState([]);
+   async function getData(){
+      const response = await fetch('http://localhost:3000');
+         console.log(response);
+         const data = await response.json();
+         setData(data.message);
+      //  setData();
+   }
+   useEffect(()=>{
+      getData();
+   },[]);
  return  (
     <>
+       <h1>{data}</h1>
        My Portfolio
     </>
   )
